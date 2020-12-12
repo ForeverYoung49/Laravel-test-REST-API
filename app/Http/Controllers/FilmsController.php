f09af10e708f96556223e6dd0efb3a71f3784cf3
ForeverYoung <ForeverYoung49@users.noreply.github.com>
2020-12-12 20:49:29 +0500

    3

--- app/Http/Controllers/FilmsController.php
+++ app/Http/Controllers/FilmsController.php
@@ -6,45 +6,134 @@ use Illuminate\Http\Request;
 use App\Models\Films;
 use App\Models\PackGenres;
 use App\Models\Genre;
+use Validator;
 
 class FilmsController extends Controller
 {
 
-    public function index()
+    public function index(Request $request)
     {
+        $sort = 'id';
+        $how = 'ASC';
+        if($request->has(['colName'])){
+            $sort = 'films.'.$request->colName;
+        }
+        if($request->has(['how'])){
+            $how = $request->how;
+        }
         $films = Films::join('pack_genres','films.id','=','pack_genres.film_id')
             ->join('genres','genres.id','=','pack_genres.genre_id')
-            ->select('films.id','films.title','films.description','films.release',Genre::raw('group_concat(`genres`.`name`) as genre'))
-            ->groupBy('films.id','films.title','films.description','films.release')
-            ->paginate(2);
-        return $films;
+            ->select('films.id','films.title','films.description','films.img','films.release',Genre::raw('group_concat(`genres`.`name`) as genre'))
+            ->groupBy('films.id','films.title','films.description','films.img','films.release')
+            ->orderBy($sort,$how);
+        if($request->has(['genre'])){
+            $genre = PackGenres::join('genres','genres.id','=','pack_genres.genre_id')
+                ->where('genres.name','=',$request->genre)
+                ->select('pack_genres.film_id')
+                ->get();
+            $films = $films->whereIn('films.id',$genre)->simplePaginate(10);
+        }
+        else {
+            $films = $films->simplePaginate(10);
+        }
+        
+        if(blank($films))
+            $films = json_encode('Not found');
+        return response()->json($films, 200);
     }
 
     public function store(Request $request)
-    {
-        return PackGenres::create($request->all());
+    {   
+        $rules = [
+            'title' => 'required|min:1|max:30',
+            'description' => 'required|min:1',
+            'img' => ['required','regex:/(\d)+|.|(?:jpeg|jpg|png|gif)/'],
+            'release' => 'required|date_format:Y-m-d',
+            'genres' => 'required|array',
+            'genres.*' => 'int'
+        ];
+
+        $validator = Validator::make($request->all(), $rules);
+
+        if($validator->fails()){
+            return response()->json($validator->errors(), 400);
+        }
+
+        Films::create([
+            'title' => $request->title,
+            'description' => $request->description,
+            'img' => $request->img,
+            'release' => $request->release
+        ]);
+
+        $maxId = Genre::select('id')->max('id');
+        $id = Films::select('id')->max('id');
+        
+        $genres = Genre::select('id')
+            ->whereIn('id',$request->genres)
+            ->get();
+        foreach($genres as $g){
+            PackGenres::create([
+                'film_id' => $id,
+                'genre_id' => $g->id
+            ]);
+        }
+
+        $newFilm = Films::join('pack_genres','films.id','=','pack_genres.film_id')
+            ->join('genres','genres.id','=','pack_genres.genre_id')
+            ->select('films.id','films.title','films.description','films.img','films.release',Genre::raw('group_concat(`genres`.`name`) as genre'))
+            ->groupBy('films.id','films.title','films.description','films.img','films.release')
+            ->where('films.id','=',$id)
+            ->first();
+
+        return response()->json($newFilm, 201);;
     }
 
     public function show($id)
     {
         $films = Films::join('pack_genres','films.id','=','pack_genres.film_id')
             ->join('genres','genres.id','=','pack_genres.genre_id')
-            ->select('films.id','films.title','films.description','films.release',Genre::raw('group_concat(`genres`.`name`) as genre'))
+            ->select('films.id','films.title','films.description','films.img','films.release',Genre::raw('group_concat(`genres`.`name`) as genre'))
             ->where('films.id','=',$id)
-            ->groupBy('films.id','films.title','films.description','films.release')
+            ->groupBy('films.id','films.title','films.description','films.img','films.release')
             ->first();
-        return $films;
+        if(is_null($films)){
+            return response()->json('Not found', 404);
+        }
+
+        return response()->json($films, 200);
     }
 
     public function update(Request $request, $id)
     {
         $film = Films::find($id);
+        if(is_null($film)){
+            return response()->json('Not found', 404);
+        }
+
+        $rules = [
+            'title' => 'min:1|max:30',
+            'description' => 'min:1',
+            'img' => ['regex:/(\d)+|.|(?:jpeg|jpg|png|gif)/'],
+            'release' => 'date_format:Y-m-d'
+        ];
+
+        $validator = Validator::make($request->all(), $rules);
+
+        if($validator->fails()){
+            return response()->json($validator->errors(), 400);
+        }
         $film->update($request->all());
-        return $film;
+        return response()->json($film, 200);
     }
 
     public function destroy($id)
     {
-        return Films::destroy($id);
+        $film = Films::find($id);
+        if(is_null($film)){
+            return response()->json('Not found', 404);
+        }
+        Films::destroy($id);
+        return response()->json('Film was deleted', 204);
     }
 }
--- database/migrations/2020_12_10_155412_create_pack_genres_table.php
+++ database/migrations/2020_12_10_155412_create_pack_genres_table.php
@@ -15,7 +15,7 @@ class CreatePackGenresTable extends Migration
     {
         Schema::create('pack_genres', function (Blueprint $table) {
             $table->unsignedBigInteger('film_id');
-            $table->foreign('film_id')->references('id')->on('films');
+            $table->foreign('film_id')->references('id')->on('films')->onDelete("cascade");;
             $table->unsignedBigInteger('genre_id');
             $table->foreign('genre_id')->references('id')->on('genres');
             $table->timestamps();
